dd1b7acb4d738813eb411523cfbcce4864266688
Gagan Chaudhary <singhgaganbtp@gmail.com>
2022-09-20 15:41:58 +0530

    14

---
+++ sort-0-1-2/sort-0-1-2.cpp
@@ -0,0 +1,13 @@
+/*
+    Time Complexity : O(N*log(N))
+    Space Complexity : O(1)
+
+    Where N is the size of the array.
+*/
+
+#include <algorithm>
+
+void sort012(int *arr, int n)
+{
+    sort(arr, arr+n);
+}
