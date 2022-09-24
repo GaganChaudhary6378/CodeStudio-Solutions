b75978e992f6a2ca243dbbd38471c53a031c2f9d
Gagan Chaudhary <singhgaganbtp@gmail.com>
2022-09-24 23:22:56 +0530

    32

---
+++ sum-of-max-and-min/sum-of-max-and-min.cpp
@@ -0,0 +1,13 @@
+int sumOfMaxMin(int arr[], int n){
+    int min=arr[0];
+    int max=arr[0];
+    int sum=0;
+    for(int i=0;i<n;i++){
+        if(arr[i]>max){
+            max=arr[i];
+        }if(arr[i]<min){
+            min=arr[i];
+        }
+    }sum=min+max;
+    return sum;
+}
