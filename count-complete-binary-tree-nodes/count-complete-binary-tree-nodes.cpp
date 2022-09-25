aa7c1e1eb2540517c89f996ce461ae9d7a903674
Gagan Chaudhary <singhgaganbtp@gmail.com>
2022-09-25 23:09:58 +0530

    37

---
+++ count-complete-binary-tree-nodes/count-complete-binary-tree-nodes.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h> 
+/************************************************************
+
+Following is the Binary Tree node class
+    
+template <typename T = int>
+class TreeNode
+{
+public:
+    T data;
+    TreeNode<T> *left;
+    TreeNode<T> *right;
+
+    TreeNode(T val)
+    {
+        this->data = val;
+        left = NULL;
+        right = NULL;
+    }
+
+    ~TreeNode()
+    {
+        if (left != NULL)
+        {
+            delete left;
+        }
+        if (right != NULL)
+        {
+            delete right;
+        }
+    }
+};
+
+************************************************************/
+
+int countNodes(TreeNode<int> *root)
+{
+    //    Write your code here.
+    if(root==NULL){
+        return 0;
+    }
+    return 1+countNodes(root->left)+countNodes(root->right);   
+}
