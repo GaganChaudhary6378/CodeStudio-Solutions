286d71be88ec46cdd8be596de6bb8e4342777a61
Gagan Chaudhary <singhgaganbtp@gmail.com>
2022-10-17 21:46:32 +0530

    61

---
+++ Segregate-Even-And-Odd-Nodes-In-a-Linked-List/segregate-even-odd.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h> 
+/****************************************************************
+
+    Following is the class structure of the Node class:
+
+        class Node
+        {
+        public:
+	        int data;
+	        Node *next;
+	        Node(int data)
+	        {
+		        this->data = data;
+		        this->next = NULL;
+	        }
+        };
+
+*****************************************************************/
+
+Node* segregateEvenOdd(Node* head)
+{
+    // Write your code here
+    if(head==NULL){
+        return head;
+    }if(head->next==NULL){
+        return head;
+    }Node* list1=new Node(0);
+    Node * list2=new Node(0);
+    Node* l1=list1;
+    Node* l2=list2;
+    while(head!=NULL){
+        if(head->data%2==0){
+            l1->next=head;
+            l1=l1->next;
+        }else{
+            l2->next=head;
+            l2=l2->next;
+        }head=head->next;
+    }
+    l1->next=list2->next;
+    l2->next=NULL;
+    return list1->next;
+}
+
