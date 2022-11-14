2200f8fef1fbb58a031d44967ab7ec029c3561e7
Gagan Chaudhary <singhgaganbtp@gmail.com>
2022-11-14 17:04:23 +0530

    100

---
+++ Wipro-interview-questions/Can-you-print/Can-you-print.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h> 
+vector < int > canYouPrint(int n) {
+    // Write your code here.
+    vector<int> ans;
+    for(int i=1;i<=n;i++){
+        ans.push_back(i);
+    }return ans;
+}
