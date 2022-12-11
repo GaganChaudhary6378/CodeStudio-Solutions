4e2b95136224bdc6e4ddde7ece575f692964bdae
Gagan Chaudhary <singhgaganbtp@gmail.com>
2022-12-11 22:57:02 +0530

    107

---
+++ LCA of three nodes/LCA of three nodes.cpp
@@ -0,0 +1,36 @@
+/**********************************************************
+    Following is the Binary Tree Node class structure.
+
+    template <typename T>
+    class BinaryTreeNode {
+    public :
+	    T data;
+	    BinaryTreeNode<T> *left;
+	    BinaryTreeNode<T> *right;
+
+	    BinaryTreeNode(T data) {
+	        this -> data = data;
+	        left = NULL;
+	        right = NULL;
+	    }
+    };
+
+***********************************************************/
+
+BinaryTreeNode<int>* lcaOfThreeNodes(BinaryTreeNode<int>* root, int node1, int node2, int node3) {
+    // Write your code here.
+    if(root==NULL){
+        return NULL;
+    }if(root->data==node1 || root->data==node2 || root->data==node3){
+        return root;
+    }BinaryTreeNode<int>* leftAns=lcaOfThreeNodes(root->left,node1,node2,node3);                    BinaryTreeNode<int>* rightAns=lcaOfThreeNodes(root->right,node1,node2,node3);
+    if(leftAns==NULL && rightAns==NULL){
+        return NULL;
+    }else if(leftAns==NULL){
+        return rightAns;
+    }else if(rightAns==NULL){
+        return leftAns;
+    }else{
+        return root;
+    }
+}
